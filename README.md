79433a00395872dcf7d0f1256515fea47a253caa
Antonio Pagano <apagano@wawand.co>
2022-05-12 07:51:07 -0400

    task: removing uneeded things

---
+++ README.md
@@ -0,0 +1,31 @@
+# Depbot
+
+Depbot is a CLI tool that identifies dependencies in a given source code repository. It aims to support multiple dependency management systems and multiple languages.
+
+## Installation
+
+To install depbot, run:
+
+```bash
+$ go install github.com/dep-bot/depbot/cmd/depbot@latest
+```
+
+Or download the appropriate binary from the releases page.
+
+## Usage
+
+Depbot is focuses on doing the analysis of dependencies within the source code. The default behavior of the command does that. Besides it, the 
+
+```bash
+$ depbot
+```
+
+And that will list the dependencies found in current directory and bellow.
+
+```bash
+[TODO] show how results look like
+```
+
+## Options
+
+[TODO] Document options
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,46 +1,46 @@
 package parser
 
-import (
-	"depbot/app/models"
-	"fmt"
-	"io/ioutil"
-	"path/filepath"
+// import (
+// 	"depbot/app/models"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"path/filepath"
 
-	"golang.org/x/mod/modfile"
-)
+// 	"golang.org/x/mod/modfile"
+// )
 
-// CanParse a path file, returns true if the file
-// can be parsed by the Parse method, this is useful
-// when looking at a directory for dependency files.
-func CanParse(path string) bool {
-	return filepath.Base(path) == "go.mod"
-}
+// // CanParse a path file, returns true if the file
+// // can be parsed by the Parse method, this is useful
+// // when looking at a directory for dependency files.
+// func CanParse(path string) bool {
+// 	return filepath.Base(path) == "go.mod"
+// }
 
-func Parse(path string) ([]models.Dependency, error) {
-	file, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("could not read %s: %w\n", path, err)
-	}
+// func Parse(path string) ([]models.Dependency, error) {
+// 	file, err := ioutil.ReadFile(path)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("could not read %s: %w\n", path, err)
+// 	}
 
-	f, err := modfile.Parse(path, file, nil)
-	if err != nil {
-		return nil, fmt.Errorf("could not read %v: %w\n", path, err)
-	}
+// 	f, err := modfile.Parse(path, file, nil)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("could not read %v: %w\n", path, err)
+// 	}
 
-	deps := []models.Dependency{}
-	for _, req := range f.Require {
-		dep := models.Dependency{
-			Library: models.Library{
-				Name:     req.Mod.Path,
-				Language: models.LanguageGo,
-			},
+// 	deps := []models.Dependency{}
+// 	for _, req := range f.Require {
+// 		dep := models.Dependency{
+// 			Library: models.Library{
+// 				Name:     req.Mod.Path,
+// 				Language: models.LanguageGo,
+// 			},
 
-			Version: req.Mod.Version,
-			Source:  path,
-		}
+// 			Version: req.Mod.Version,
+// 			Source:  path,
+// 		}
 
-		deps = append(deps, dep)
-	}
+// 		deps = append(deps, dep)
+// 	}
 
-	return deps, nil
-}
+// 	return deps, nil
+// }
