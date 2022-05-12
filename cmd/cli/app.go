f43a531e19428e0fefebbf1f4fa8bae83a039cf3
Antonio Pagano <apagano@wawand.co>
2022-05-12 08:10:28 -0400

    task: abstracting CLI things to be better tested

---
+++ cmd/cli/app.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"io"
+)
+
+type App struct {
+	IO
+}
+
+func (app *App) Main(ctx context.Context, pwd string, args []string) error {
+	if app == nil {
+		return fmt.Errorf("app is nil")
+	}
+
+	// if len(args) == 0 {
+	// 	return app.Usage(app.Stdout())
+	// }
+
+	// if app.Commands == nil {
+	// 	app.Commands = map[string]Commander{}
+	// }
+
+	// cmd, ok := app.Commands[args[0]]
+	// if !ok {
+	// 	return fmt.Errorf("command %q not found", args[0])
+	// }
+
+	return nil //cmd.Main(ctx, pwd, args[1:])
+}
+
+func (app *App) Usage(w io.Writer) error {
+	fmt.Fprintln(w, "Usage: depbot [options]")
+	fmt.Fprintln(w, "---------------")
+
+	return nil
+}
--- cmd/depbot/main.go
+++ cmd/depbot/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/depbot/depbot/cmd/cli"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	go func() {
+		time.Sleep(50 * time.Millisecond)
+		cancel()
+	}()
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	app := &cli.App{}
+	err = app.Main(ctx, pwd, os.Args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	<-ctx.Done()
 }
