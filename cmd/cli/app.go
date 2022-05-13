080ca129391cbcd29bfa212ee370c5aa50ec3e20
Ezequiel Galindo <egalindo@wawand.co>
2022-05-13 13:38:32 -0500

    Calls the findDpendencies from the app.go

--- cmd/cli/app.go
+++ cmd/cli/app.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io"
+
+	"github.com/godepbot/depbot/internal/gomodules"
 )
 
 type App struct {
@@ -18,6 +20,15 @@ func (app *App) Main(ctx context.Context, pwd string, args []string) error {
 	if len(args) > 0 {
 		return app.Usage(app.Stdout())
 	}
+
+	deps, err := gomodules.FindDependencies(pwd)
+
+	if err != nil {
+		fmt.Println("Error is", err)
+		return err
+	}
+
+	fmt.Println("Total dependencies found:", len(deps))
 
 	// if app.Commands == nil {
 	// 	app.Commands = map[string]Commander{}
@@ -34,6 +45,7 @@ func (app *App) Main(ctx context.Context, pwd string, args []string) error {
 func (app *App) Usage(w io.Writer) error {
 	fmt.Fprintln(w, "Usage: depbot [options]")
 	fmt.Fprintln(w, "---------------")
+	fmt.Fprintln(w, "Hola Bry")
 
 	return nil
 }
