792d2c9e464b1524c82ed4bf9cbd9fdfce28532e
Ezequiel Galindo <egalindo@wawand.co>
2022-05-15 16:56:55 -0500

    Adds unit tests for the gomodules package

--- internal/gomodules/gomodules_test.go
+++ internal/gomodules/gomodules_test.go
@@ -1,13 +1,15 @@
-package gomodules
+package gomodules_test
 
 import (
 	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
+
+	"github.com/godepbot/depbot/internal/gomodules"
 )
 
-// d, err := os.TempDir()
+// d, err := t.TempDir()
 // Write go.mod en directory
 // Write package/go.mod
 // Write package/p2/go.mod
@@ -26,11 +28,7 @@ var FileContent = `
 
 func Test_SingleDependency(t *testing.T) {
 
-	tmp := t.TempDir()
-
-	fmt.Println("Tmp directory:", tmp)
-
-	file, err := os.Create(tmp + "/" + "go.mod")
+	file, err := os.Create(t.TempDir() + "/" + "go.mod")
 
 	if err != nil {
 		t.Logf("got an error but should be nil, error: %v ", err)
@@ -46,7 +44,7 @@ func Test_SingleDependency(t *testing.T) {
 		return
 	}
 
-	dependencies, err := FindDependencies(tmp)
+	dependencies, err := gomodules.FindDependencies(file.Name())
 	if err != nil {
 		t.Logf("got an error but should be nil, error : %v ", err.Error())
 		t.Fail()
@@ -58,4 +56,67 @@ func Test_SingleDependency(t *testing.T) {
 		t.Fail()
 		return
 	}
+}
+
+func Test_MultipleDependencies(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	errWriteFile := ioutil.WriteFile(tmpDir+"/go.mod", []byte(FileContent), 0644)
+	if errWriteFile != nil {
+		t.Logf("got an error but should be nil, error : %v ", errWriteFile.Error())
+		t.Fail()
+		return
+	}
+
+	newDirectoriesPath := tmpDir + "/package/v2"
+	packagePath := tmpDir + "/package"
+
+	err := os.MkdirAll(newDirectoriesPath, os.ModePerm)
+
+	if err != nil {
+		fmt.Println("Error creating directories:", err)
+		return
+	}
+
+	errWriteFile = ioutil.WriteFile(packagePath+"/go.mod", []byte(FileContent), 0644)
+	if errWriteFile != nil {
+		t.Logf("got an error but should be nil, error : %v ", errWriteFile.Error())
+		t.Fail()
+		return
+	}
+
+	errWriteFile = ioutil.WriteFile(newDirectoriesPath+"/go.mod", []byte(FileContent), 0644)
+
+	if errWriteFile != nil {
+		t.Logf("got an error but should be nil, error : %v ", errWriteFile.Error())
+		t.Fail()
+		return
+	}
+
+	dependencies, err := gomodules.FindDependencies(tmpDir)
+
+	if len(dependencies) != 9 {
+		t.Logf("got %v, but was expected %v", len(dependencies), 9)
+		t.Fail()
+		return
+	}
+
+}
+
+func Test_NoDependency(t *testing.T) {
+	tmp := t.TempDir()
+
+	dependencies, err := gomodules.FindDependencies(tmp)
+
+	if err != nil {
+		t.Logf("Error finding the dependencies : %v ", err.Error())
+		t.Fail()
+		return
+	}
+
+	if len(dependencies) > 0 {
+		t.Logf("got %v, but was expected %v", len(dependencies), 0)
+		t.Fail()
+	}
+
 }
