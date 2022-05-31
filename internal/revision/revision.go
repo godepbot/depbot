9a947156bd0d8110a7d3b64d2cd6ac0df989bdca
Ezequiel Galindo <egalindo@wawand.co>
2022-05-31 16:10:21 -0500

    creates a new function to get the head data

--- internal/revision/revision.go
+++ internal/revision/revision.go
@@ -15,40 +15,54 @@ const (
 )
 
 func FindLatestHash(pwd string) (string, error) {
-	path := filepath.Join(pwd, git, head)
-	bytes, err := os.ReadFile(path)
+
+	headData, err := readHead(pwd)
 
 	if err != nil {
 		fmt.Println("Error is :)", err)
 		return "", err
 	}
 
-	headContent := strings.TrimSpace(string(bytes))
-
 	/// HEAD points to a branch that is the current branch.
-	if strings.Contains(headContent, refsPath) {
-		components := strings.Split(headContent, ": ")
-
-		if !(len(components) >= expectedComponentes) {
-			return "", fmt.Errorf("Oops! No hash available.")
-		}
-
-		branchPath := components[1]
-		fullBranchPath := filepath.Join(pwd, git, branchPath)
-
-		bytes, err = os.ReadFile(fullBranchPath)
-
-		if err != nil {
-			return "", fmt.Errorf("Oops! Something happened! %v", err)
-		}
-
-		return string(bytes), nil
+	if strings.Contains(headData, refsPath) {
+		return hashFromBranch(pwd, headData)
 	}
 
 	/// HEAD points to a commit in particular.
-	if len(headContent) > 0 {
-		return headContent, nil
+	if len(headData) > 0 {
+		return headData, nil
 	}
 
 	return "", fmt.Errorf("Oops! No hash available.")
+}
+
+func readHead(pwd string) (string, error) {
+	headPath := filepath.Join(pwd, git, head)
+	bytes, err := os.ReadFile(headPath)
+
+	if err != nil {
+		fmt.Println("Error is :)", err)
+		return "", err
+	}
+
+	return strings.TrimSpace(string(bytes)), nil
+}
+
+func hashFromBranch(pwd string, headContent string) (string, error) {
+	components := strings.Split(headContent, ": ")
+
+	if !(len(components) >= expectedComponentes) {
+		return "", fmt.Errorf("Oops! No hash available.")
+	}
+
+	branchPath := components[1]
+	fullBranchPath := filepath.Join(pwd, git, branchPath)
+
+	bytes, err := os.ReadFile(fullBranchPath)
+
+	if err != nil {
+		return "", fmt.Errorf("Oops! Something happened! %v", err)
+	}
+
+	return string(bytes), nil
 }
